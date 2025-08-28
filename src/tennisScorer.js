0349dde514b15274efe4606f549c8afc0c7186ba
Israel Antezana Rojas <israelantezana@gmail.com>
2025-08-28 10:12:59 -0400

    refactoring: ahora se encapsula en objetos la funcionalidad del scorer

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,5 +1,17 @@
-function showScore() {
-  return "Love - Love";
+class TennisScorer {
+  constructor() {
+    this.player1Points = 0;
+  }
+  showScore() {
+    if (this.player1Points === 1) {
+      return "15 - Love";
+    } else if (this.player1Points === 2) {
+      return "30 - Love";
+    }
+    return "Love - Love";
+  }
+  player1Scores() {
+    this.player1Points++;
+  }
 }
-
-export default showScore;
+export default TennisScorer;
--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,8 +1,22 @@
-import showScore from "./tennisScorer.js";
+import TennisScorer from "./tennisScorer.js";
 
 describe("TennisScorer", () => {
   //Empieza sin puntos     --> "Love - Love"
   it("game empieza con ambos en cero", () => {
-    expect(showScore()).toEqual("Love - Love");
+    const scorer = new TennisScorer();
+    expect(scorer.showScore()).toEqual("Love - Love");
+  });
+
+  it("Jugador 1 marca un punto ---> 15 0", () => {
+    const scorer = new TennisScorer();
+    scorer.player1Scores();
+    expect(scorer.showScore()).toEqual("15 - Love");
+  });
+
+  it("Jugador 1 marca 2 puntos ---> 30 0", () => {
+    const scorer = new TennisScorer();
+    scorer.player1Scores();
+    scorer.player1Scores();
+    expect(scorer.showScore()).toEqual("30 - Love");
   });
 });
