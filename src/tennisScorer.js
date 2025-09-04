d1ef65d958d35331677d874b5fed071390155457
Robert <robertortizarias@gmail.com>
2025-09-04 01:49:59 -0400

    feat: marcador llega a douce

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -15,4 +15,25 @@ export default class TennisScorer {
   showScore() {
     return `${this.scoreNames[this.scores[0]]} - ${this.scoreNames[this.scores[1]]}`;
   }
+  showScore() {
+  const [p1, p2] = this.scores;
+  if (p1 >= 3 && p2 >= 3) {
+    if (p1 === p2)
+     { return 'Deuce';}
+    if (p1 - p2 === 1)
+      {return 'Advantage Player 1';}
+    if (p2 - p1 === 1)
+      {return 'Advantage Player 2';}
+    if (p1 - p2 >= 2)
+      {return 'Game for Player 1';}
+    if (p2 - p1 >= 2)
+      {return 'Game for Player 2';}
+  }
+  if (p1 >= 4 && p1 - p2 >= 2)
+    {return 'Game for Player 1';}
+  if (p2 >= 4 && p2 - p1 >= 2)
+    {return 'Game for Player 2';}
+  return `${this.scoreNames[Math.min(p1, 3)]} - ${this.scoreNames[Math.min(p2, 3)]}`;
+}
+
 }
