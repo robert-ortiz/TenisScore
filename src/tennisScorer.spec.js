b95459f464b96ef256afa0fd8374a1801061d42b
Robert <robertortizarias@gmail.com>
2025-09-04 01:27:10 -0400

    feat: test rojo jugador 1 anota primer punto

--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,10 +1,14 @@
 import TennisScorer from './tennisScorer.js';
 
 describe('TennisScorer', () => {
-  //Empieza sin puntos     --> "Love - Love"
   it('game empieza con ambos en cero', () => {
     const scorer = new TennisScorer();
     expect(scorer.showScore()).toEqual('Love - Love');
   });
 
+  it('Jugador 1 anota', () => {
+    const scorer = new TennisScorer();
+    scorer.player1Scores();
+    expect(scorer.showScore()).toEqual('15 - Love');
+  });
 });
