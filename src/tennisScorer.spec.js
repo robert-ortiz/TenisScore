13296c7284dae2a19b73bc1a6dc464e4f49ec4b9
Robert <robertortizarias@gmail.com>
2025-09-04 01:42:19 -0400

    feat:test rojo marcador llega a douce

--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -17,4 +17,14 @@ describe('TennisScorer', () => {
     scorer.player2Scores();
     expect(scorer.showScore()).toEqual('15 - 15');
   });
+  it('Jugador 2 anota y llega a Deuce', () => {
+    const scorer = new TennisScorer();
+    scorer.player1Scores();
+    scorer.player2Scores();
+    scorer.player1Scores();
+    scorer.player2Scores();
+    scorer.player1Scores();
+    scorer.player2Scores();
+    expect(scorer.showScore()).toEqual('Deuce');
+  });
 });
