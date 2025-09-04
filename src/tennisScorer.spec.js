fe8bb25fa8dbe082184a8115dd70ea4e86b15749
Robert <robertortizarias@gmail.com>
2025-09-04 01:07:43 -0400

    first commit:test en rojo para comenzar marcador

--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,29 +1,10 @@
-import TennisScorer from "./tennisScorer.js";
+import TennisScorer from './tennisScorer.js';
 
-describe("TennisScorer", () => {
+describe('TennisScorer', () => {
   //Empieza sin puntos     --> "Love - Love"
-  it("game empieza con ambos en cero", () => {
+  it('game empieza con ambos en cero', () => {
     const scorer = new TennisScorer();
-    expect(scorer.showScore()).toEqual("Love - Love");
+    expect(scorer.showScore()).toEqual('Love - Love');
   });
 
-  it("Jugador 1 marca un punto y el jugador en cero", () => {
-    const scorer = new TennisScorer();
-    scorer.player1Scores();
-    expect(scorer.showScore()).toEqual("15 - Love");
-  });
-
-  it("Jugador 1 marca 2 puntos y jugador 2 en cero", () => {
-    const scorer = new TennisScorer();
-    scorer.player1Scores();
-    scorer.player1Scores();
-    expect(scorer.showScore()).toEqual("30 - Love");
-  });
-  it("El Jugador 1 marca 3 puntos y jugador 2 en 0", () => {
-    const scorer = new TennisScorer();
-    scorer.player1Scores();
-    scorer.player1Scores();
-    scorer.player1Scores();
-    expect(scorer.showScore()).toEqual("40 - Love");
-  });
 });
