c863fc2f2f3e5cef2300174a5eccddb3ead80255
Robert <robertortizarias@gmail.com>
2025-09-04 01:29:57 -0400

    feat: jugador 1 anota primer punto

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,7 +1,11 @@
 export default class TennisScorer {
   constructor() {
-    this.scores = [0, 0]; // [player1, player2]
+    this.scores = [0, 0];
     this.scoreNames = ['Love', '15', '30', '40'];
+  }
+
+  player1Scores() {
+    this.scores[0]++;
   }
 
   showScore() {
